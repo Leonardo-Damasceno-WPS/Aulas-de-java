75049763bdcb0cb61195359fc4de2e48d0032d18
Leonardo-Damasceno-WPS <130994439+Leonardo-Damasceno-WPS@users.noreply.github.com>
2023-09-01 21:35:08 -0300

    Polimorfismo

--- Polimorfismo/src/polimorfismo/TestaPolimorfismo.java
+++ Polimorfismo/src/polimorfismo/TestaPolimorfismo.java
@@ -7,6 +7,9 @@ public class TestaPolimorfismo extends Aluno {
 	public static void main(String [] args) {
 		pessoa p = new pessoa("Anakin Skywalker","CPF de Pessoa");
 		mostraDados(p);
+		
+		Aluno a = new Aluno("Yoda","CPF de Pessoa", "ra");
+		mostraDados(a);
 	}
 	
 	private static void mostraDados(pessoa pessoa) {
